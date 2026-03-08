2b4311418198dc6a427fa4500f919c46c40183e3
pashamray <pashamray@gmail.com>
2026-03-08 18:10:39 +0100

    fix: improve variable naming and logging for electricity data processing

--- processor/DSMRElectricityProcessor.py
+++ processor/DSMRElectricityProcessor.py
@@ -12,27 +12,30 @@ class DSMRElectricityProcessor(AbstractProcessor):
 
     def __call__(self, data) -> None:
         sn = data.EQUIPMENT_IDENTIFIER.value
-        t1 = data.ELECTRICITY_USED_TARIFF_1
-        t2 = data.ELECTRICITY_USED_TARIFF_2
-        current = data.CURRENT_ELECTRICITY_USAGE
-        returned = data.CURRENT_ELECTRICITY_DELIVERY
+        used_tariff_1 = data.ELECTRICITY_USED_TARIFF_1
+        used_tariff_2 = data.ELECTRICITY_USED_TARIFF_2
+        usage = data.CURRENT_ELECTRICITY_USAGE
+        delivery = data.CURRENT_ELECTRICITY_DELIVERY
 
         logger.info(
-            f"electricity sn={sn} "
-            f"t1={t1.value}{t1.unit} "
-            f"t2={t2.value}{t2.unit} "
-            f"now={current.value}{current.unit} "
-            f"returned={returned.value}{returned.unit}"
+            f"electricity "
+            f"sn={sn} "
+            f"used_tariff_1={used_tariff_1.value} {used_tariff_1.unit} "
+            f"used_tariff_2={used_tariff_2.value} {used_tariff_2.unit} "
+            f"usage={usage.value} {usage.unit} "
+            f"delivery={delivery.value} {delivery.unit}"
         )
 
         if self.storage:
             self.storage.write(
                 "electricity",
-                {"sn": sn},
                 {
-                    "t1": t1.value,
-                    "t2": t2.value,
-                    "current": current.value,
-                    "returned": returned.value,
+                    "sn": sn,
+                },
+                {
+                    "used_tariff_1": used_tariff_1.value,
+                    "used_tariff_2": used_tariff_2.value,
+                    "usage": usage.value,
+                    "delivery": delivery.value,
                 },
             )
